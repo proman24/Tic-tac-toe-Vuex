c003d5cb6e4f6f1fa90a6309dd24bc8884776379
proman24 <mathur.geetanshu887@gmail.com>
2020-05-02 19:34:03 +0530

    cell functional

---
+++ src/components/Cell.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="cell" @click="makeMove" :class="cellClass">
+    <span id="text">{{ this.player ? this.player : ""}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Cell',
+    data(){
+        return{
+            player:null
+        }
+    },
+    computed:{
+        cellClass(){
+            return this.player
+        }
+    },
+    methods:{
+        makeMove:function(){
+            this.player='X'
+        }
+    }
+}
+</script>
+
+<style>
+#cell{
+    position:relative;
+    height: 200px;
+    width:200px;
+    border:1px solid black;
+}
+.X{
+    background: burlywood;
+}
+.O{
+    background: cadetblue;
+}
+#text{
+    font-size: 92px;
+    top:50%;
+    left:50%;
+    position: absolute;
+    transform: translate(-50%,-50%);
+}
+</style>
---
+++ src/views/Board.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <cell/>
+      <cell/>
+      <cell/>
+  </div>
+</template>
+
+<script>
+import Cell from '@/components/Cell.vue'
+export default {
+    components:{
+        Cell
+    }
+}
+</script>
+
+<style>
+
+</style>
