55c6afc77792d3fcd87b1fc1253e55f3d45b73bc
proman24 <mathur.geetanshu887@gmail.com>
2020-05-03 08:25:15 +0530

    making moves functional

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,27 +1,39 @@
 <template>
-  <div>
-      <!-- <cell v-for="row in 9" :key="row" player="X">
-      </cell> -->
+  <div id="board">
+      <!-- <cell :player="player" @click.native="checkPlayer" :top="2" :left="2" /> -->
       <div v-for="row in 3" :key="row">
           <div v-for="col in 3" :key="col" >
-               <cell player="O" :top="row" :left="col" />
+               <cell :player="board[row-1][col-1]" @click.native="checkPlayer(row,col)" :top="row" :left="col" />
             </div>
-      </div                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    >
-
-      <!-- <cell player="X" top=1 left=1 />
-      <cell player="O" top=1 left=2 />
-      <cell player="X" top=1 left=3 />
-      <cell player="O" top=2 left=1 />
-      <cell player="X" top=2 left=2 />
-      <cell player="O" top=2 left=3 /> -->
+        </div>
   </div>
 </template>
 
 <script>
 import Cell from '@/components/Cell.vue'
 export default {
+    name:"board",
     components:{
         Cell
+    },
+    data(){
+        return{
+            chance:0,
+            board:[["", "", ""], ["", "", ""], ["", "", ""]]
+        }
+    },
+    methods:{
+        checkPlayer:function(row,col){
+            if (this.chance)
+                this.board[row-1][col-1] = "X"
+            else
+                this.board[row-1][col-1] = "O"
+            this.chance= ! this.chance
+            // console.log("prev", this.board[row-1][col-1])
+            // this.board[row-1][col-1] = (this.board[row-1][col-1] == "O") ? "X" : "O"
+            // console.log("After", this.board[row-1][col-1])
+            this.$forceUpdate();
+        }
     }
 }
 </script>
